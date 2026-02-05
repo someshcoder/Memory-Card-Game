20e609cfa7575f2fa420939c3e7e03fa7c3964c6
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-02-05 22:52:43 +0530

    Initial project setup with React + TypeScript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl text-blue-400 font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
